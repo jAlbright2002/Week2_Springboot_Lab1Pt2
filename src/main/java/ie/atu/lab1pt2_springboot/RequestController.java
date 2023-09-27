a945f0432cd5303bc041287e44ad94bf0474b4b6
JamesA <G00379271@atu.ie>
2023-09-27 14:59:43 +0100

    Create method hello and return "hello world"

--- src/main/java/ie/atu/lab1pt2_springboot/RequestController.java
+++ src/main/java/ie/atu/lab1pt2_springboot/RequestController.java
@@ -1,7 +1,14 @@
 package ie.atu.lab1pt2_springboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RequestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello world";
+    }
+
 }
